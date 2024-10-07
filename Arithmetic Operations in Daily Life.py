d8be40012b2ee27074c67cf4c9a2fd8fc815c6e3
PBreland <phil.breland42@gmail.com>
2024-10-07 15:33:30 -0500

    Module Assignment

---
+++ Arithmetic Operations in Daily Life.py
@@ -0,0 +1,22 @@
+## Task 1: Grocery Store Math
+
+bread = 2
+peanut_butter = 4 
+jelly = 3
+
+total = bread + peanut_butter + jelly
+print(total)
+
+
+## Task 2: Bank Interest
+
+account = 10000
+rate = .07
+
+interest = account * rate
+print(interest)
+
+total = account + interest
+print(total)
+
+# Not sure if its formatted completely right, did it all on my own!
---
+++ Showcase Your Dance Moves!.py
@@ -0,0 +1,19 @@
+# Task 1: Code Correction
+
+weather = "sunny"
+if weather == "sunny" :
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+# I had to indent the print lines to make the code work.
+
+# Task 2: Your Mood Today
+    
+mood = input("What is your mood today?")
+
+if mood == "happy":
+    print("That is good to hear!")
+elif mood == "sad":
+    print("Hope your day gets better!")
+
+    
